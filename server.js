734a41533f2a77524299eb58bd330cf066a4cea6
caas <casvanneerven@icloud.com>
2025-03-12 11:30:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-const xlsx = require('xlsx');
 const app = express();
 const port = 3000;
 
@@ -13,67 +13,69 @@ app.use(session({
     saveUninitialized: true
 }));
 
-const excelFilePath = path.join(__dirname, 'data.xlsx');
+const db = new sqlite3.Database(':memory:');
 
-function readExcelFile() {
-    const workbook = xlsx.readFile(excelFilePath);
-    const adminsSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Admins']);
-    const employeesSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Medewerkers']);
-    const recordsSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Geregistreerde tijden']);
-    return { adminsSheet, employeesSheet, recordsSheet };
-}
+db.serialize(() => {
+    db.run("CREATE TABLE admins (username TEXT, password TEXT)");
+    db.run("CREATE TABLE employees (username TEXT, password TEXT)");
+    db.run("CREATE TABLE records (userId TEXT, startTime TEXT, endTime TEXT, location TEXT)");
 
-function writeExcelFile(data) {
-    const workbook = xlsx.readFile(excelFilePath);
-    workbook.Sheets['Admins'] = xlsx.utils.json_to_sheet(data.adminsSheet);
-    workbook.Sheets['Medewerkers'] = xlsx.utils.json_to_sheet(data.employeesSheet);
-    workbook.Sheets['Geregistreerde tijden'] = xlsx.utils.json_to_sheet(data.recordsSheet);
-    xlsx.writeFile(workbook, excelFilePath);
-}
+    db.run("INSERT INTO admins (username, password) VALUES ('cas', 'Kaassticks11!')");
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    const { adminsSheet, employeesSheet } = readExcelFile();
-    const admin = adminsSheet.find(a => a.Gebruikersnaam === username && a.Wachtwoord === password);
-    const employee = employeesSheet.find(e => e.Gebruikersnaam === username && e.Wachtwoord === password);
-    if (admin) {
-        req.session.user = admin;
-        res.send({ role: 'admin', message: 'Login successful' });
-    } else if (employee) {
-        req.session.user = employee;
-        res.send({ role: 'employee', message: 'Login successful' });
-    } else {
-        res.status(401).send('Invalid credentials');
-    }
+    db.get("SELECT * FROM admins WHERE username = ? AND password = ?", [username, password], (err, admin) => {
+        if (admin) {
+            req.session.user = admin;
+            res.send({ role: 'admin', message: 'Login successful' });
+        } else {
+            db.get("SELECT * FROM employees WHERE username = ? AND password = ?", [username, password], (err, employee) => {
+                if (employee) {
+                    req.session.user = employee;
+                    res.send({ role: 'employee', message: 'Login successful' });
+                } else {
+                    res.status(401).send('Invalid credentials');
+                }
+            });
+        }
+    });
 });
 
 app.post('/addUser', (req, res) => {
-    if (!req.session.user || req.session.user.Gebruikersnaam !== 'cas') {
+    if (!req.session.user || req.session.user.username !== 'cas') {
         return res.status(403).send('Unauthorized');
     }
     const { name } = req.body;
-    const data = readExcelFile();
-    data.employeesSheet.push({ Gebruikersnaam: name, Wachtwoord: 'password' });
-    writeExcelFile(data);
-    res.send('User added');
+    db.run("INSERT INTO employees (username, password) VALUES (?, ?)", [name, 'password'], (err) => {
+        if (err) {
+            res.status(500).send('Error adding user');
+        } else {
+            res.send('User added');
+        }
+    });
 });
 
 app.post('/registerTime', (req, res) => {
     const { userId, startTime, endTime, location } = req.body;
-    const data = readExcelFile();
-    data.recordsSheet.push({ Gebruikersnaam: userId, Starttijd: startTime, Eindtijd: endTime, Locatie: location });
-    writeExcelFile(data);
-    res.send('Time registered');
+    db.run("INSERT INTO records (userId, startTime, endTime, location) VALUES (?, ?, ?, ?)", [userId, startTime, endTime, location], (err) => {
+        if (err) {
+            res.status(500).send('Error registering time');
+        } else {
+            res.send('Time registered');
+        }
+    });
 });
 
 app.get('/getRecords', (req, res) => {
-    if (!req.session.user || req.session.user.Gebruikersnaam !== 'cas') {
+    if (!req.session.user || req.session.user.username !== 'cas') {
         return res.status(403).send('Unauthorized');
     }
-    const { recordsSheet } = readExcelFile();
-    res.json(recordsSheet);
+    db.all("SELECT * FROM records", (err, rows) => {
+        res.json(rows);
+    });
 });
 
 app.listen(port, () => {
